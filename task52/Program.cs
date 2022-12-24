a933177b444ed4a029f6c606ffecbb2324057755
Kailar0000 <kailar0000@gmail.com>
2022-12-24 12:29:06 +0300

    Task52

---
+++ task52/Program.cs
@@ -0,0 +1,41 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(-10, 11); // [-10, 10]
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void Medium(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int medium = 0 , sum= 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum=sum+matrix[i,j];
+    }
+    medium =sum/matrix.GetLength(0);
+    Console.WriteLine($"Столбец: {j+1} Средниие: {medium}");
+    }
+}
+
+Console.Clear();
+int sizex = new Random().Next(1, 11);
+int sizey = new Random().Next(1, 11);
+int[,] matrix = new int[sizex, sizey];
+Console.WriteLine("Начальный массив:");
+InputMatrix(matrix);
+PrintMatrix(matrix);
+Medium(matrix);
